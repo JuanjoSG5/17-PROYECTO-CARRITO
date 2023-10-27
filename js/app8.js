10dce001efac07a883544a3a66a321714cc703d8
Juan Jose Sanchez Gonzalez <113671548+JuanjoSG5@users.noreply.github.com>
2023-10-27 13:06:04 +0200

    Added session storage  app8.js
    TODO -> refactor

--- js/app8.js
+++ js/app8.js
@@ -2,6 +2,7 @@ const primaryButtons = document.querySelectorAll('.button-primary')
 const list = document.querySelector("#lista-carrito")
 const emptyList = document.getElementById("vaciar-carrito")
 const removeButton = document.querySelector('.remove-button')
+const cartItems = [];
 
 primaryButtons.forEach(function(elem){
     elem.addEventListener('click',event => {
@@ -9,6 +10,11 @@ primaryButtons.forEach(function(elem){
         addToCarrito(dataID)
     })
 })
+
+
+emptyList.addEventListener("click", clearCart)
+
+// Add to carrito new elements 
 
 function addToCarrito(id){
     let element = document.createElement("tr")
@@ -26,12 +32,17 @@ function addToCarrito(id){
     }
 }
 
-emptyList.addEventListener("click", () => {
-    while (list.lastElementChild !== null) {
-        list.removeChild(list.lastElementChild);
-    }
-    
-})
+
+
+
+function clearCart() {
+    emptyList.addEventListener("click", () => {
+        const tbody = list.querySelector("tbody")
+        while (tbody.lastElementChild) {
+            tbody.removeChild(tbody.lastElementChild)
+        }
+    })
+}
 
 function addOneToValuesIfExists(id, card) {
     let cantidad = document.getElementsByClassName(`cantidad-${id}`)[0]
@@ -39,23 +50,119 @@ function addOneToValuesIfExists(id, card) {
         let precio = cantidad.parentElement.children[2]
         cantidad.textContent = parseInt(cantidad.textContent) + 1
         precio.textContent = parseInt(precio.textContent) + parseInt(card.lastElementChild.children[3].querySelector(".u-pull-right").textContent)
+        addToCartAndSessionStorage(id,card)
     }
 }
 
 
 function removeOne(id, card) {
-    let cantidad = document.getElementsByClassName(`cantidad-${id}`)[0]
+    let cantidad = document.getElementsByClassName(`cantidad-${id}`)[0];
     if (cantidad) {
         let row = cantidad.parentElement;
-        let precio = cantidad.parentElement.children[2]
-        cantidad.textContent = parseInt(cantidad.textContent) - 1
-        precio.textContent = parseInt(precio.textContent) - parseInt(card.lastElementChild.children[3].querySelector(".u-pull-right").textContent)
+        let precio = cantidad.parentElement.children[2];
+        cantidad.textContent = parseInt(cantidad.textContent) - 1;
+        precio.textContent = parseInt(precio.textContent) - parseInt(card.lastElementChild.children[3].querySelector(".u-pull-right").textContent);
+        removeFromCartAndSessionStorage(id)
         if (cantidad.textContent == 0) {
             row.parentElement.removeChild(row);
+            
         }
     }
 }
 
+// Session storage functions
+
+function addToCartAndSessionStorage(id, card) {
+    const existingItem = cartItems.find(item => item.id === id);
+
+    if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice += parseInt(card.lastElementChild.children[3].querySelector(".u-pull-right").textContent)
+    } else {
+        const newItem = {
+            id: id,
+            name: card.lastElementChild.firstElementChild.textContent,
+            price: parseInt(card.lastElementChild.children[3].querySelector(".u-pull-right").textContent),
+            quantity: 1,
+            totalPrice: parseInt(card.lastElementChild.children[3].querySelector(".u-pull-right").textContent),
+            image: card.firstElementChild.getAttribute('src'), // Include the image URL
+        };
+        cartItems.push(newItem);
+    }
+    sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
+    console.log(cartItems)
+}
+
+function removeFromCartAndSessionStorage(id) {
+    const itemIndex = cartItems.findIndex(item => item.id === id);
+
+    if (itemIndex !== -1) {
+        const item = cartItems[itemIndex];
+        item.quantity--;
+
+        if (item.quantity <= 0) {
+            cartItems.splice(itemIndex, 1);
+        } else {
+            item.totalPrice -= item.price;
+        }
+        sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
+    }
+}
+
+function clearCartAndSessionStorage() {
+    cartItems.length = 0; 
+    sessionStorage.removeItem("cartItems"); 
+}
+
+function handleDOMContentLoaded() {
+    const storedCartItems = JSON.parse(sessionStorage.getItem("cartItems"));
+    if (storedCartItems) {
+        cartItems.push(...storedCartItems);
+        const tableBody = document.querySelector("#lista-carrito tbody");
+        clearCart()
+        cartItems.forEach(item => {
+            const row = createRowForItem(item);
+            tableBody.appendChild(row);
+        });
+    }
+}
+
+
+function createRowForItem(item) {
+    const row = document.createElement("tr");
+    row.appendChild(createTableCellWithImage(item.image, item.name));
+    row.appendChild(createTableCellWithText(item.name));
+    row.appendChild(createTableCellWithText(item.price));
+    row.appendChild(createTableCellWithText(item.quantity));
+    row.appendChild(createRemoveButton(item));
+    return row;
+}
+
+function createTableCellWithImage(imageSrc, altText) {
+    const cell = document.createElement("td");
+    cell.innerHTML = `<img src="${imageSrc}" alt="${altText}" width="150" height="100">`;
+    return cell;
+}
+
+function createTableCellWithText(text) {
+    const cell = document.createElement("td");
+    cell.textContent = text;
+    return cell;
+}
+
+function createRemoveButton(item) {
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", () => {
+        removeItemFromCart(item.id);
+    });
+    const cell = document.createElement("td");
+    cell.appendChild(removeButton);
+    return cell;
+}
+
+
+// Add new elements to cart
 
 function createNewElement(id, card, element) {
     let image = document.createElement("img")
@@ -64,6 +171,8 @@ function createNewElement(id, card, element) {
     let cantidad = document.createElement("th")
     let remove = document.createElement("button")
     addPropertiesToElements(cantidad, id, image, card, nombre, precio, remove)
+    
+    addToCartAndSessionStorage(id,card)
     addNewElements(element, image, nombre, precio, cantidad, remove)
 }
 
@@ -95,11 +204,15 @@ function addRemoveProperties(remove, id,card) {
 }
 
 function addNewElements(...args) {
-    args[0].appendChild(args[1])
-    args[0].appendChild(args[2])
-    args[0].appendChild(args[3])
-    args[0].appendChild(args[4])
-    args[0].appendChild(args[5])
+    let element = args[0]
+    element.appendChild(args[1])
+    element.appendChild(args[2])
+    element.appendChild(args[3])
+    element.appendChild(args[4])
+    element.appendChild(args[5])
+
     list.lastElementChild.appendChild(args[0])
 }
+
+
 
